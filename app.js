181594ca922006e00a351fd22039237d0c2366fb
Rain <pyc0422@gmail.com>
2022-07-11 19:06:43 -0500

    Making applications interactive with state

--- app.js
+++ app.js
@@ -1,16 +1,79 @@
+// class TodoListItem extends React.Component {
 
-var GroceryListItem = (props) => (
+//   // A `constructor` method is expected on all ES6 classes
+//   // When React instantiates the component,
+//   // it will pass `props` to the constructor
+//   constructor(props) {
+//     // Equivalent to ES5's React.Component.call(this, props)
+//     super(props);
+//   }
+
+//   // Every class component must have a `render` method
+//   // Stateless functional components are pretty much just this method
+//   render() {
+
+//     // `props` is no longer passed as an argument,
+//     // but instead accessed with `this.props`
+//     return (
+//       <li>{this.props.todo}</li>
+//     );
+
+//   }
+
+// }
+
+// // Update our `TodoList` to use the new `TodoListItem` component
+// // This can still be a stateless function component!
+// var TodoList = (props) => (
+//   <ul>
+//     {props.todos.map(todo =>
+//       <TodoListItem todo={todo} />
+//     )}
+//   </ul>
+// );
+
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+//   </div>
+// );
+
+// ReactDOM.render(<App /> , document.getElementById("app"));
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  };
+
+  onListItemMouseMove() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'none'
+    }
+    return(
+      <li style={style} onMouseOver={this.onListItemMouseMove.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+var ListItem = (props) => (
   <ul>
-    <li>{props.groceryItems[0]}</li>
-    <li>{props.groceryItems[1]}</li>
+    {props.groceryListItem.map(item =>
+      <GroceryListItem item={item} />
+    )}
   </ul>
 );
 
-var GroceryList= () => (
-  <div>
-    <GroceryListItem groceryItems={['3 apple', '4 banana']}/>
-  </div>
 
+var GroceryList= () => (
+  <ListItem groceryListItem={['apple','banana']}/>
 );
 
 
